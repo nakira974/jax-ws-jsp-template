a5d29c348b28d73f69e125165c328be072c6f7ef
nakira974 <maxime.loukhal@gmail.com>
2023-05-16 19:20:20 +0200

    Update dependencies

--- src/main/java/fr/aura/markandweb/gena_server/servlets/HelloServlet.java
+++ src/main/java/fr/aura/markandweb/gena_server/servlets/HelloServlet.java
@@ -3,13 +3,15 @@ package fr.aura.markandweb.gena_server.servlets;
 import java.io.*;
 import java.net.URL;
 
-import fr.aura.markandweb.gena_server.servlets.soap.EchoService;
+import fr.aura.merkandweb.gena_server.controllers.xml.EchoService;
+import fr.aura.merkandweb.gena_server.controllers.xml.EchoServicePortType;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import jakarta.xml.ws.Service;
 
+import jakarta.xml.ws.WebServiceException;
 import org.jetbrains.annotations.NotNull;
 
 import javax.xml.namespace.QName;
@@ -23,22 +25,35 @@ public class HelloServlet extends HttpServlet {
 
 
     public void doGet(@NotNull  HttpServletRequest request, @NotNull HttpServletResponse response) throws IOException {
-        EchoService echoService = new EchoService();
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
         out.println("<html><body>");
         out.println("<h1>" + title + "</h1>");
         try {
             // Create a URL object pointing to the WSDL of the Webservice
-            URL wsdlUrl = new URL("http://localhost:8080/EchoService?wsdl");
+            URL wsdlUrl = new URL("http://localhost:8083/EchoService?wsdl");
 
             // Create a QName object representing the name of the Webservice
-            QName serviceName = new QName("http://localhost:8083/echo", "EchoService");
+            QName serviceName = new QName("http://localhost:8083/echo", "echoRequest");
 
             // Create a Service object using the Service.create() method
-            Service service = Service.create(wsdlUrl, serviceName);
+            final EchoService service;
+            try{
+                service   = (EchoService) Service.create(wsdlUrl, serviceName);
 
+            }catch (WebServiceException ex){
+                System.err.println(ex.getMessage());
+                return;
+            }
 
+            // Get the EchoPortType from the Service
+            EchoServicePortType echoPort = service.getPort(EchoServicePortType.class);
+
+            // Call the echo() method on the EchoPortType
+            String echoedString = echoPort.echo("Hello World!");
+
+            // Print the result
+            out.println("<p>Result: " + echoedString + "</p>");
             // Process the SOAP response
             System.out.println(response);
 
